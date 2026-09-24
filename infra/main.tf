ef63060af915d065e0477b1ff8724a8671560811
criscal3 <c.calderonb@pucp.edu.pe>
2026-09-24 17:40:11 -0500

    Fix role reference in AWS Lambda function

--- infra/main.tf
+++ infra/main.tf
@@ -72,7 +72,7 @@ resource "aws_cloudwatch_log_group" "funcion" {
 
 resource "aws_lambda_function" "app" {
   function_name = var.nombre_aplicacion
-  role          = local.arn_rol_ejecucioness
+  role          = local.arn_rol_ejecucion
   package_type  = "Image"
   image_uri     = local.imagen_inicial
   architectures = ["x86_64"]
